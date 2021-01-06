fc4accd0be8d066a62b0c2a00f6d46a536e1b7b7
meltem <meltemkaraagac19@gmail.com>
2021-01-06 13:59:31 -0500

    add

--- src/weather/views.py
+++ src/weather/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from decouple import config
+import requests
+from pprint import pprint
+
 
 def index(request):
-    return render(request, "weather/index.html")
+    # 'api.openweathermap.org/data/2.5/weather?q={}&appid=&units=metric'.format("Berlin")
+    url = config("BASE_URL")
+    city = "Berlin"
+    r = requests.get(url.format(city))
+    content = r.json()
+    # print(type(a))
+    # pprint(a)
+    context = {
+        "city": city,
+        "temp": content["main"]["temp"],
+        "description": content["weather"][0]["description"],
+        "icon": content["weather"][0]["icon"]
+    }
+    return render(request, "weather/index.html", context)
